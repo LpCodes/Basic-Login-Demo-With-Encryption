e99aa95e1c31d20bb76eda02cbb46a4133c959ce
LP <lovelesh_p@zohomail.in>
2023-09-19 12:24:11 +0530

    Mods - Restructured

--- Simpleloginmanager.py
+++ Simpleloginmanager.py
@@ -2,125 +2,74 @@ import json
 from pathlib import Path
 import sys
 import cryptocode
-import json
 
+DATA_FILE = "cd.json"
+ENCRYPTION_KEY = "123"
 
-def chekifoldfileexists():
-    """Checking here if old file exits if not creating"""
+def check_or_create_data_file():
+    """Check if the data file exists; if not, create it."""
     cur_dir = Path.cwd()
-    file = Path(f"{cur_dir}/cd.json")
-    # print(file)
+    file = cur_dir / DATA_FILE
+
     if file.exists():
-        print("OLD Json Data file already exits")  # load existing data
-        with open("cd.json", "r") as jsonFile:
-            mydic = json.load(jsonFile)
-            return mydic
+        print("OLD Json Data file already exists")
+        return load_data_from_file(file)
     else:
-        # create new
-        print("Creating new json data file as old data was not present")
-        mydic = {}
-        return mydic
+        print("Creating a new JSON data file as old data was not present")
+        return {}
 
+def load_data_from_file(file):
+    """Load data from the given JSON file."""
+    with open(file, "r") as json_file:
+        return json.load(json_file)
 
-def Register_User(data):
-    # Get usernames and passwords
-    uname = input("enter uname\n\n")
-    while uname in data.keys():
-        print("Username already exits plz provide a new one")
-        uname = input("enter uname\n\n")
-        if uname not in data.keys():
+def save_data_to_file(data, file):
+    """Save data to the given JSON file."""
+    with open(file, "w") as json_file:
+        json.dump(data, json_file, indent=4, sort_keys=True)
+
+def register_user(data):
+    """Register a new user."""
+    while True:
+        uname = input("Enter username:\n")
+        if uname in data:
+            print("Username already exists. Please choose a different one.")
+        else:
             break
 
-    pwd = input("enter pwd\n\n")
+    pwd = input("Enter password:\n")
+    encrypted_pwd = encrypt_password(pwd)
+    return uname, encrypted_pwd
 
-    return uname, pwd
+def encrypt_password(password):
+    """Encrypt the given password."""
+    return cryptocode.encrypt(password, ENCRYPTION_KEY)
 
-
-def Encrpyt_pwd(Password):
-    """Here Encrpting password"""
-    return cryptocode.encrypt(Password, "123")
-
-
-def adding_data_to_dic(Olddata, Username, Encrptedpass):
-    Olddata[Username] = Encrptedpass
-    # print(Olddata)
-    return Olddata
-
-
-def addind_data_to_json(data):
-
-    with open("cd.json", "w") as jsonFile:
-        json.dump(data, jsonFile, indent=4, sort_keys=True)
-
-
-def verify_login():
-    "verifying login here"
+def verify_login(data):
+    """Verify user login."""
     print("#" * 50 + "Login" + "#" * 50)
-    uname1 = input("Please Enter login Username\n\n")
-    pwd2 = input("Please Enter Login Password\n\n")
+    uname = input("Please Enter login Username:\n")
+    pwd = input("Please Enter Login Password:\n")
 
     try:
-        with open("cd.json", "r") as file:
-            data = json.load(file)
-            # print(data)
-
-            try:
-                if cryptocode.decrypt(data[uname1], "123") == pwd2:
-                    print("login success")
-                else:
-                    print("Login fail")
-            except KeyError:
-                Olddata = chekifoldfileexists()
-                user_responce = input(
-                    "Username does not exits\n\nDo you want to create a new user\n\nPress Y to create New User or any other key to exit\n\n"
-                )
-
-                if user_responce.capitalize() == "Y":
-
-                    Username, Password = Register_User(Olddata)
-
-                    Encrptedpass = Encrpyt_pwd(Password)
-
-                    updated_data = adding_data_to_dic(Olddata, Username, Encrptedpass)
-
-                    addind_data_to_json(updated_data)
-                    print("User added succussfully - Rerun Login to try again")
-                else:
-                    sys.exit()
-    except FileNotFoundError as e:
-        Olddata = chekifoldfileexists()
-        user_responce = input(
-            "Username does not exits\n\nDo you want to create a new user\n\nPress Y to create New User or any other key to exit\n\n"
-        )
-
-        if user_responce.capitalize() == "Y":
-
-            Username, Password = Register_User(Olddata)
-
-            Encrptedpass = Encrpyt_pwd(Password)
-
-            updated_data = adding_data_to_dic(Olddata, Username, Encrptedpass)
-
-            addind_data_to_json(updated_data)
-
-            print("User added succussfully - Rerun Login to try again")
+        encrypted_pwd = data[uname]
+        if cryptocode.decrypt(encrypted_pwd, ENCRYPTION_KEY) == pwd:
+            print("Login successful")
+        else:
+            print("Login failed")
+    except KeyError:
+        print("Username does not exist")
+        user_response = input("Do you want to create a new user? (Y/N):\n")
+        if user_response.capitalize() == "Y":
+            username, password = register_user(data)
+            encrypted_password = encrypt_password(password)
+            data[username] = encrypted_password
+            save_data_to_file(data, DATA_FILE)
+            print("User added successfully. Please rerun the login to try again.")
         else:
             sys.exit()
 
-
 if __name__ == "__main__":
-
-    Olddata = chekifoldfileexists()
-    print("Current json data is as below \n\n", Olddata)
-
-    verify_login()
-
-    # Username, Password = Register_User(Olddata)
-
-    # # print(Username, Password)
-
-    # Encrptedpass = Encrpyt_pwd(Password)
-
-    # updated_data = adding_data_to_dic(Olddata, Username, Encrptedpass)
-
-    # addind_data_to_json(updated_data)
+    data = check_or_create_data_file()
+    print("Current JSON data:\n", data)
+    verify_login(data)
--- readme.md
+++ readme.md
@@ -1,13 +1,36 @@
 ## Basic Login Demo With Encryption Offline Storage without database
 
-![Visitor count](https://shields-io-visitor-counter.herokuapp.com/badge?page=https://github.com/LpCodes/Basic-Login-Demo-With-Encryption)
 
-## Importing required libraries
-The script requires following libraries to be installed on the system in order to function properly:
+This is a simple user authentication system implemented in Python. It allows users to register with a unique username and password, which are then stored securely in a JSON file. Users can then log in using their credentials.
 
-1. pathlib for handling file system paths and operations
-2. sys for interacting with the interpreter
-3. cryptocode for encrypting the password
-4. json for reading and writing JSON files.
-## Functionality
-This script allows users to register a new account by creating a new JSON file named 'cd.json' or adding new data to an existing one, and logging into the account by providing the correct credentials. The script also encrypts the password before storing it in the JSON file for added security.
+## Features
+
+- User registration with a unique username and password.
+- Passwords are securely encrypted before storage.
+- User data is stored in a JSON file.
+- User authentication during login.
+
+## Prerequisites
+
+- Python 3.x
+- Additional package: `cryptocode` (You can install it using `pip install cryptocode`)
+
+## Usage
+
+1. Clone the repository or download the Python script (`user_authentication.py`) to your local machine.
+
+2. Open a terminal or command prompt and navigate to the directory containing the script.
+
+3. Run the script using the following command:
+
+
+4. Follow the on-screen instructions to perform user registration and login.
+
+## Configuration
+
+- You can modify the following constants in the script to customize its behavior:
+
+- `DATA_FILE`: The name of the JSON data file where user data is stored.
+- `ENCRYPTION_KEY`: The encryption key used to securely store and retrieve passwords.
+
+
